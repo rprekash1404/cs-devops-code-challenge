a0f44da01e6c8259e5265f6b7fb2e158cb6f7214
Ratnesh Prakash <RPrakash3@slb.com>
2022-05-18 10:56:02 +0530

    cs code challenge

---
+++ src/main/java/com/example/cscodecrack/CsCodeCrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cscodecrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsCodeCrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsCodeCrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cscodecrack/CsCodeCrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cscodecrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsCodeCrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
